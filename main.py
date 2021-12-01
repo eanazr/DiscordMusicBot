d320dbcbdc1c81fcdbc7c537ee17a409c536d8a2
Azizan Az-Raihan <48229932+eanazr@users.noreply.github.com>
2021-12-01 17:28:46 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import keep_alive
+import discord
+from discord.ext import commands
+import music
+
+cogs = [music]
+
+client = commands.Bot(command_prefix='!',
+intents = discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+    
+keep_alive.keep_alive()
+
+client.run("YourDiscordToken")
